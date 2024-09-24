4da21ac43e196f23b44b8d8c6f3327f83b120972
Rob Cameron <cannikin@fastmail.com>
2024-09-24 14:15:01 -0700

    Form building working

--- web/src/components/ArticleCell/ArticleCell.jsx
+++ web/src/components/ArticleCell/ArticleCell.jsx
@@ -1,3 +1,5 @@
+import Article from 'src/components/Article'
+
 export const QUERY = gql`
   query FindArticleQuery($id: Int!) {
     article: post(id: $id) {
@@ -14,9 +16,9 @@ export const Loading = () => <div>Loading...</div>
 export const Empty = () => <div>Empty</div>
 
 export const Failure = ({ error }) => (
-  <div style={{ color: 'red' }}>Error: {error?.message}</div>
+  <div style={{ color: 'red' }}>Error: {error.message}</div>
 )
 
 export const Success = ({ article }) => {
-  return <div>{JSON.stringify(article)}</div>
+  return <Article article={article} />
 }
