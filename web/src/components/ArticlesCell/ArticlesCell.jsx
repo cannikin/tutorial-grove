522c05adb5989f5004d885acb66310a67245afa4
Rob Cameron <cannikin@fastmail.com>
2024-09-24 13:13:27 -0700

    Built out home page and article detail

---
+++ web/src/components/ArticlesCell/ArticlesCell.jsx
@@ -0,0 +1,36 @@
+import { Link, routes } from '@redwoodjs/router'
+
+export const QUERY = gql`
+  query ArticlesQuery {
+    articles: posts {
+      id
+      title
+      body
+      createdAt
+    }
+  }
+`
+
+export const Loading = () => <div>Loading...</div>
+
+export const Empty = () => <div>Empty</div>
+
+export const Failure = ({ error }) => (
+  <div style={{ color: 'red' }}>Error: {error?.message}</div>
+)
+
+export const Success = ({ articles }) => {
+  return (
+    <>
+      {articles.map((article) => (
+        <article key={article.id}>
+          <header>
+            <Link to={routes.article({ id: article.id })}>{article.title}</Link>
+          </header>
+          <p>{article.body}</p>
+          <div>Posted at: {article.createdAt}</div>
+        </article>
+      ))}
+    </>
+  )
+}
